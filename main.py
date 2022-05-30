bff5a3429a79b5d620f29dcb2d1343ced204e80e
Jonathan Jesús Herrera López <jonathan_herrera117@hotmail.com>
2022-05-30 03:15:25 -0500

    Added menu logic for the script

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import random
 import uuid
 import json
 import jsbeautifier
+import sys
 
 # ? SECCIÓN DE CONSTANTES
 
@@ -14,8 +15,9 @@ SAMPLE_PATH = 'dataset/testSample'# * Ruta del folder para las imagenes de testi
 RESULTS_PATH = 'results' # * Ruta del folder donde se guardaran los archivos con los resultados de cada entrenamiento
 
 # ? Configuración de los puntos sobre la imagen
-IMG_QUANTITY = 100 # ! Valores --> 5 para testing | 50 para entrenamiento
-POINT_QUANTITY = 50 # ! Valores --> 5 para testing | 30 o más para entrenamiento
+am_gen_quantity = 1
+img_quantity = 100 # ! Valores --> 5 para testing | 50 para entrenamiento
+point_quantity = 50 # ! Valores --> 5 para testing | 30 o más para entrenamiento
 IMG_SIZE = 27 # ? Se maneja solo un valor ya que son imagenes cuadradas de la forma LxL px
 REGION_VALUE = 5 # ? Valor que maneja la distancia hacia dentro de la imagen considerada como región de interes
 POINT_COLOR = [7, 49, 255] # ! --> Rojo ; Proposito de testing
@@ -28,7 +30,7 @@ def generateConfiguration():
   * @return: Lista con las configuraciones de los puntos
   """
   config = []
-  for i in range(POINT_QUANTITY):
+  for i in range(point_quantity):
     config.append([random.randint(0 + REGION_VALUE, IMG_SIZE - REGION_VALUE), random.randint(0 + REGION_VALUE, IMG_SIZE - REGION_VALUE)])
   return config
 
@@ -43,7 +45,7 @@ def imageBinarization(img, pointsConfig):
   """
 
   # ? Se inicializa la lista que representará la imagen en formato binario
-  binaryIMG = np.zeros((POINT_QUANTITY, ), dtype=int)
+  binaryIMG = np.zeros((point_quantity, ), dtype=int)
   openedImage = cv2.imread(img)
 
   # ? Referencia => https://stackoverflow.com/questions/9780632/how-do-i-determine-if-a-color-is-closer-to-white-or-black#:~:text=The%20former%20is%20easy%3A%20convert,closer%20to%20white%20(255).
@@ -68,7 +70,7 @@ def readImages(folder, pointsConfig):
   images = []
   imgCount = 0
   for filename in os.listdir(f'{DATA_PATH}/{folder}'):
-      if imgCount < IMG_QUANTITY:
+      if imgCount < img_quantity:
           imgCount += 1
           images.append(imageBinarization(f'{DATA_PATH}/{folder}/{filename}', pointsConfig))
       else:
@@ -158,8 +160,8 @@ def saveResults(id, AM, pointsConfig, quality, efficiency):
 
   JSON = {
     'id': id,
-    'pointsQuantity': POINT_QUANTITY,
-    'imageQuantity': IMG_QUANTITY,
+    'pointsQuantity': point_quantity,
+    'imageQuantity': img_quantity,
     'pointsConfig': pointsConfig,
     'AM': AM.tolist(),
     'quality': quality.tolist(),
@@ -178,9 +180,7 @@ def saveResults(id, AM, pointsConfig, quality, efficiency):
 
 # ? --------------------------------------------------------------------------------------------------------------------------------------------------
 
-# ! Sección de ejecución -----------------------------------------------------------------------------------------------------------------------------
-if __name__ == '__main__':
-  print("INICIO")
+def main():
 
   # * Generando ID para el archivo de la memoria asociativa
   ID = str(uuid.uuid4())
@@ -190,31 +190,99 @@ if __name__ == '__main__':
   
   # * Generando la configuracion de los puntos
   pointsConfig = generateConfiguration()
-  print("CONFIGURACIÓN DE LOS PUNTOS GENERADA")
 
   # ? Referencia al método map --> https://realpython.com/python-map-function/
   # * Generando la matriz de imagenes
   imgMatrix = list(map(lambda folder: readImages(folder, pointsConfig), imgFolders))
-  print("MATRIZ DE IMAGENES BINARIZADAS GENERADA")
 
   # * Inicializando la memoria asociativa
-  AM = np.zeros((len(imgMatrix), POINT_QUANTITY), dtype=int)
-  print("MEMORIA ASOCIATIVA INICIALIZADA")
+  AM = np.zeros((len(imgMatrix), point_quantity), dtype=int)
 
   # * Entrenando la memoria asociativa
   AM = learningPhase(imgMatrix, AM)
-  print("ENTRENAMIENTO DE LA MEMORIA ASOCIATIVA FINALIZADO")
 
   # # * Caculando calidad de la memoria asociativa
   binaryAM = list(map(binaryzeAM, np.copy(AM)))
-  quality = sum(np.transpose(binaryAM)) / POINT_QUANTITY
+  quality = sum(np.transpose(binaryAM)) / point_quantity
 
   # * Probando porcentaje de eficiencia de la memoria asociativa
   efficiencyRate = testEfficiencyRate(AM, pointsConfig)
-  print("EFICIENCIA DE LA MEMORIA ASOCIATIVA CALCULADA")
 
   # # * Guardando la memoria asociativa, la configuracion de los puntos y un ID (con proposito de distinguir entre cada entrenamiento)
   saveResults(ID, AM, pointsConfig, quality, efficiencyRate)
-  print("MEMORIA ASOCIATIVA GUARDADA")
 
-  print("MODELO FINALIZADO")
+# ? --------------------------------------------------------------------------------------------------------------------------------------------------
+
+# ! SECCIÓN PARA MANEJO DEL MENÚ
+
+HELP_MENU = str(
+  "\n--> LEARNING MATRIX STEINBUCH <--\n\n" +
+  "\t Args:\n\n" +
+  "\t\t 1 Arg --> Cantidad de memorias asociativas que se desean genenrar\n\n" +
+  "\t\t 2 Arg --> Cantidad imagenes a tomar por clase (número comprendido entre 1 y 100)\n\n" +
+  "\t\t 3 Arg --> Cantidad puntos a generar\n\n"
+)
+
+# ! Funciones para el manejo del menú usado en base a la cantidad de argumentos recibidos por consola
+
+def getFirstArgCondition(args): return args[0] >= 1
+def getSecondArgCondition(args): return (args[1] >= 1 and args[1] <= 100)
+
+def getHelp(_):
+  print(HELP_MENU)
+  global am_gen_quantity 
+  am_gen_quantity = 0
+
+def genAM(args):
+  if (getFirstArgCondition(args)):
+    global am_gen_quantity 
+    am_gen_quantity = args[0]
+  else:
+    print("ERROR: Argumento invalido, verifique la ayuda con --help")
+
+def genAM_IMG(args):
+  if (getFirstArgCondition(args) and getSecondArgCondition(args)):
+    global am_gen_quantity,img_quantity
+    am_gen_quantity = args[0]
+    img_quantity = args[1]
+  else:
+    print("ERROR: Argumentos invalidos, verifique la ayuda con --help")
+
+def genAM_IMG_POINTS(args):
+  if (getFirstArgCondition(args) and getSecondArgCondition(args) and args[2] >= 1):
+    global am_gen_quantity,img_quantity,point_quantity
+    am_gen_quantity = args[0]
+    img_quantity = args[1]
+    point_quantity = args[2]
+  else:
+    print("ERROR: Argumentos invalidos, verifique la ayuda con --help")
+
+ARGS_MENU = {
+  "0" : getHelp,
+  "1" : genAM,
+  "2" : genAM_IMG,
+  "3" : genAM_IMG_POINTS
+}
+
+# ! Sección de ejecución -----------------------------------------------------------------------------------------------------------------------------
+if __name__ == '__main__':
+
+  args = sys.argv[1:]
+  nArgs = len(args)
+
+  if (nArgs and nArgs <=3) :
+    help_condition = args[0] == '--help'
+    ARGS_MENU["0" if help_condition else str(nArgs)](... if help_condition else list(map(lambda arg: int(arg), args)))
+  elif nArgs:
+    print("Cantidad de argumentos invalida, verifique la ayuda con --help")
+
+  if (am_gen_quantity > 1): 
+    print("ENTRENANDO MEMORIAS ASOCIATIVAS SOLICITADAS")
+    for i in range(am_gen_quantity):
+      main()
+      print(".......")
+    print("PROCESO TERMINADO")
+  elif (am_gen_quantity == 1):
+    print("ENTRENANDO MEMORIA ASOCIATIVA")
+    main()
+    print("PROCESO TERMINADO")
